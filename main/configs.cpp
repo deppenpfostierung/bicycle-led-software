bc1b4d7a841708d101718c4ac7ddd590884aa2c8
CommanderRedYT <ccommanderred@gmail.com>
2025-04-01 21:12:02 +0200

    Test ci

--- main/configs.cpp
+++ main/configs.cpp
@@ -13,6 +13,8 @@ std::string defaultHostname()
     if (const auto result = wifi_stack::get_default_mac_addr())
         return std::format("{}_{:02x}{:02x}{:02x}", APP_NAME, result->at(3), result->at(4), result->at(5));
     else
+
+
         ESP_LOGE(TAG, "get_default_mac_addr() failed: %.*s", result.error().size(), result.error().data());
     return APP_NAME;
 }
