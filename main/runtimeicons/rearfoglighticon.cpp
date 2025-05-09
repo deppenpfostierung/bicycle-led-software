72697026cb28c70de9e89200de82cff7466de82f
CommanderRedYT <ccommanderred@gmail.com>
2025-05-09 23:02:29 +0200

    Implement the proper icon

--- main/runtimeicons/rearfoglighticon.cpp
+++ main/runtimeicons/rearfoglighticon.cpp
@@ -16,14 +16,26 @@ void RearFoglightIcon::redraw(espgui::TftInterface &tft, const bool force, const
         return;
     }
 
-    const auto color = forceColor.value_or(m_isVisible ? espgui::TFT_GREEN : espgui::TFT_BLACK);
+    m_lastVisible = m_isVisible;
 
-    tft.fillRect(m_x, m_y, getWidth(), getHeight(), color);
+    const auto color = forceColor.value_or(m_isVisible ? espgui::TFT_RED : espgui::TFT_BLACK);
+
+    const auto radius = getHeight() / 2;
+
+    tft.drawArc(m_x + radius, m_y + radius, radius, radius, 0, 180, color, espgui::TFT_BLACK);
+    tft.drawLine(m_x + radius, m_y, m_x + radius, m_y + getHeight(), color);
+
+    tft.drawLine(m_x + radius + 10, m_y, m_x + radius + 10, m_y + getHeight(), color);
+    tft.drawLine(m_x + radius + 5, m_y + radius - 5, m_x + radius + 15, m_y + radius - 5, color);
+    tft.drawLine(m_x + radius + 5, m_y + radius, m_x + radius + 15, m_y + radius, color);
+    tft.drawLine(m_x + radius + 5, m_y + radius + 5, m_x + radius + 15, m_y + radius + 5, color);
 }
 
 uint16_t RearFoglightIcon::getWidth()
 {
-    return 20;
+    const auto radius = getHeight() / 2;
+
+    return radius + 15;
 }
 
 uint16_t RearFoglightIcon::getHeight()
