43ac33a3b8feb1f6c9551aa463313d5b0e2d83f3
CommanderRedYT <ccommanderred@gmail.com>
2025-05-17 01:26:19 +0200

    Do AI on clang-format to help with stupid CI

--- format.sh
+++ format.sh
@@ -16,11 +16,27 @@ if [[ ! -d "./main" ]]; then
 fi
 
 # print clang-format version
-clang-format --version
+clang_format_version=$(clang-format --version) # "clang-format version x.y.z"
+clang_format_command="clang-format"
 
-echo "Running clang-format on all .h and .cpp files in the ./main directory, excluding the ./main/icons directory."
+# check if clang-format version is 19 or higher
+if [[ $clang_format_version =~ [0-9]+ ]]; then
+    clang_format_version_number=${BASH_REMATCH[0]}
+    if (( clang_format_version_number < 19 )); then
+        # check if clang-format-19 is installed
+        if ! command -v clang-format-19 &> /dev/null; then
+            echo "clang-format version $clang_format_version_number is less than 19 and clang-format-19 is not installed."
+            exit 1
+        else
+            echo "clang-format version $clang_format_version_number is less than 19, using clang-format-19."
+            clang_format_command="clang-format-19"
+        fi
+    fi
+fi
+
+echo "Running $(which $clang_format_command) on all .h and .cpp files in the ./main directory, excluding the ./main/icons directory."
 
 # run clang-format. use the '.clang-format' file in the current directory.
 find ./main -iname '*.h' -o -iname '*.cpp' -o -iname '*.c' | \
     grep -v './main/icons/' | \
-    xargs clang-format --Werror --verbose --style=file -i
+    xargs $clang_format_command --Werror --verbose --style=file -i
